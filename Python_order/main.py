fafdb0c0328ee2eff0e631937c933bbd7e85c76b
emihel <e.mihel@yandex.ru>
2021-11-03 19:35:00 +0300

    Final commit

--- Python_order/main.py
+++ Python_order/main.py
@@ -13,8 +13,21 @@ def main():
     global text
     for w in words:
         try:
-            word, key = map(str, w.split("-"))
-            text = re.sub(f'(?i){word.strip()}(?=\W)', key.strip(), text)
+            if '/-' in w:
+                word, key = map(str, w.split("/-"))
+                if not (word.strip() in words_keep):
+                    text = re.sub(f'(?i){word.strip()}(?=\W)', f'{key.strip()}', text)
+
+            elif '\-' in w:
+                word, key = map(str, w.split("\-"))
+                if not (word.strip() in words_keep):
+                    text = re.sub(f'(?<=^\b|\W){word.strip()}(?=\W|\b$)', f'{key.strip()}', text)
+
+            elif '/r-' in w:
+                regexp, key = map(str, w.split("/r-"))
+                if not (regexp.strip() in words_keep):
+                    print(regexp.strip())
+                    text = re.sub(regexp.strip(), key.strip(), text)
         except:
             pass
 
@@ -29,8 +42,15 @@ def add_words():
     path_words["text"] = "Выбранный файл: "+words_filename
     with open(words_filename, encoding="utf-8") as g:
         words = g.read().split("\n")
-        print(words)
 
+
+def add_words_keep():
+    global words_keep
+
+    words_filename = askopenfilename()
+    path_words["text"] = "Выбранный файл: "+words_filename
+    with open(words_filename, encoding="utf-8") as g:
+        words_keep = g.read().split("\n")
 
 
 def add_main_text():
@@ -44,15 +64,22 @@ def add_main_text():
 
 text = ""
 words = []
+words_keep = []
 text_filename = ""
 root = tk.Tk()
-root.geometry('460x400')
+root.geometry('460x600')
 
 btn_words = tk.Button(root, text="Выбрать словарь для замены",
-                   command=add_words).place(x=30, y=20)
+                   command=add_words).place(x=30, y=370)
 path_words = tk.Label(state="disabled")
 path_words["text"] = "Выбранный файл:"
-path_words.place(x=30, y=50)
+path_words.place(x=30, y=400)
+
+btn_words_keep = tk.Button(root, text="Выбрать словарь зарезервивованных слов",
+                   command=add_words_keep).place(x=30, y=10)
+path_words = tk.Label(state="disabled")
+path_words["text"] = "Выбранный файл:"
+path_words.place(x=30, y=40)
 
 btn_text = tk.Button(root, text="Выбрать текст для замены",
                   command=add_main_text).place(x=30, y=90)
